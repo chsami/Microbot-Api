cd799518e35ea8f43314b9169ea3b64abb3c29fc
chsami <sami.chkhachkhi@gmail.com>
2024-09-03 06:07:25 +0200

    bump client to 1.4.5

--- MicrobotApi/Controllers/FileController.cs
+++ MicrobotApi/Controllers/FileController.cs
@@ -59,7 +59,7 @@ public class FileController : Controller
     [HttpGet("client")]
     public async Task<IActionResult> Client()
     {
-        return Ok("1.4.4");
+        return Ok("1.4.5");
     }
     
     [HttpGet("html")]
