d8578cf90fd1db3411e471c7741f773fe0d52250
chsami <sami.chkhachkhi@gmail.com>
2024-09-25 21:05:05 +0200

    BUMP 1.4.9

--- MicrobotApi/Controllers/FileController.cs
+++ MicrobotApi/Controllers/FileController.cs
@@ -59,7 +59,7 @@ public class FileController : Controller
     [HttpGet("client")]
     public async Task<IActionResult> Client()
     {
-        return Ok("1.4.8");
+        return Ok("1.4.9");
     }
     
     [HttpGet("html")]
