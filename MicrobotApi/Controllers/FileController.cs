f5a6ec9877a95c7a04fccf7512ddffea9f073904
chsami <sami.chkhachkhi@gmail.com>
2024-10-16 17:44:10 +0200

    bump 1.5.7

--- MicrobotApi/Controllers/FileController.cs
+++ MicrobotApi/Controllers/FileController.cs
@@ -59,7 +59,7 @@ public class FileController : Controller
     [HttpGet("client")]
     public async Task<IActionResult> Client()
     {
-        return Ok("1.5.6");
+        return Ok("1.5.7");
     }
     
     [HttpGet("html")]
