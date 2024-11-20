124d15c3c3d96fb4743462bf5fd26a87c7c100e4
chsami <sami.chkhachkhi@gmail.com>
2024-11-20 14:40:05 +0100

    1.6.1

--- MicrobotApi/Controllers/FileController.cs
+++ MicrobotApi/Controllers/FileController.cs
@@ -59,7 +59,7 @@ public class FileController : Controller
     [HttpGet("client")]
     public async Task<IActionResult> Client()
     {
-        return Ok("1.6.0");
+        return Ok("1.6.1");
     }
     
     [HttpGet("html")]
