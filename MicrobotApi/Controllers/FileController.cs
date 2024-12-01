31342543de3528c369700e48da7b470d876e89e7
chsami <sami.chkhachkhi@gmail.com>
2024-12-01 06:41:56 +0100

    1.6.4.2

--- MicrobotApi/Controllers/FileController.cs
+++ MicrobotApi/Controllers/FileController.cs
@@ -59,7 +59,7 @@ public class FileController : Controller
     [HttpGet("client")]
     public async Task<IActionResult> Client()
     {
-        return Ok("1.6.4.1");
+        return Ok("1.6.4.2");
     }
     
     [HttpGet("html")]
