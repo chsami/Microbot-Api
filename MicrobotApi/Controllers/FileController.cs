d26bad919258067145963e2e98b32c64c6d00aac
chsami <sami.chkhachkhi@gmail.com>
2025-04-23 14:08:30 +0200

    update client endpoint response to version 1.8.7

--- MicrobotApi/Controllers/FileController.cs
+++ MicrobotApi/Controllers/FileController.cs
@@ -59,7 +59,7 @@ public class FileController : Controller
     [HttpGet("client")]
     public async Task<IActionResult> Client()
     {
-        return Ok("1.8.6");
+        return Ok("1.8.7");
     }
     
     [HttpGet("html")]
