1f81746825b9a450857ae40008861dc73c4fca49
g-mason0 <19415334+g-mason0@users.noreply.github.com>
2025-05-08 10:23:40 -0400

    chore: bump to 1.8.8.1

--- MicrobotApi/Controllers/FileController.cs
+++ MicrobotApi/Controllers/FileController.cs
@@ -59,7 +59,7 @@ public class FileController : Controller
     [HttpGet("client")]
     public async Task<IActionResult> Client()
     {
-        return Ok("1.8.8");
+        return Ok("1.8.8.1");
     }
     
     [HttpGet("html")]
