097aedbde264b7733464506ae60afad90ce44bee
g-mason0 <19415334+g-mason0@users.noreply.github.com>
2025-06-10 11:44:07 -0400

    chore: bump to 1.9.0

--- MicrobotApi/Controllers/FileController.cs
+++ MicrobotApi/Controllers/FileController.cs
@@ -59,7 +59,7 @@ public class FileController : Controller
     [HttpGet("client")]
     public async Task<IActionResult> Client()
     {
-        return Ok("1.8.9.3");
+        return Ok("1.9.0");
     }
     
     [HttpGet("html")]
