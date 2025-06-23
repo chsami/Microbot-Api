0a24eaf412ee0957859fb5fddc80283b8313397e
chsami <sami.chkhachkhi@gmail.com>
2025-06-23 22:39:10 +0200

    fix cors

--- MicrobotApi/Program.cs
+++ MicrobotApi/Program.cs
@@ -14,6 +14,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationInsightsTelemetry();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder
+                .WithOrigins(
+                    "https://themicrobot.com")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        });
+});
+
 StripeConfiguration.ApiKey = builder.Configuration["Stripe:ApiKey"];
 
 // Add services to the container.
